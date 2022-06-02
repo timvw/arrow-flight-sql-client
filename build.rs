fb4e1ef959e9746983a625d9951ccd80ee79ca73
Tim Van Wassenhove <tim@timvw.be>
2022-06-02 14:28:05 +0200

    only generate when proto file changes

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     //intellij does not easily find types when not adding code to src
@@ -10,16 +10,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let out_dir = "./src";
 
-    {
-        println!("cargo:rerun-if-changed=proto/Flight.proto");
+    let path = Path::new("./proto/Flight.proto");
+    if path.exists() {
+        println!("cargo:rerun-if-changed=./proto/Flight.proto");
         tonic_build::configure()
             .out_dir(out_dir)
             .file_descriptor_set_path(original_out_dir.join("flight_descriptor.bin"))
             .compile(&["proto/Flight.proto"], &["proto"])?;
     }
 
-    {
-        println!("cargo:rerun-if-changed=proto/FlightSql.proto");
+    let path = Path::new("./proto/FlightSql.proto");
+    if path.exists() {
+        println!("cargo:rerun-if-changed=./proto/FlightSql.proto");
         tonic_build::configure()
             .out_dir(out_dir)
             .file_descriptor_set_path(original_out_dir.join("flight_sql_descriptor.bin"))
